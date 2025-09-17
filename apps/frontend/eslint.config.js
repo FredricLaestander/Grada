60cd69aad375c71864fe64086f573b8cb78fffc6
Fredric Laestander <fredriclaestander@gmail.com>
2025-09-17 20:51:14 +0200

    solve tanstack eslint config

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,28 +1,10 @@
 //  @ts-check
-import pluginJsxA11y from 'eslint-plugin-jsx-a11y'
-import globals from 'globals'
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
 export default [
   ...tanstackConfig,
   {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    languageOptions: { globals: globals.browser },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
-  pluginJsxA11y.flatConfigs.recommended,
-  {
-    // turn off legacy rule that requires React to be imported in every file
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-    },
-  },
-  {
-    ignores: ['dist'],
+    ignores: ['eslint.config.js', 'prettier.config.js'],
   },
 ]
