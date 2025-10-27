89dc105d7f515861890f72f418cf29df73be59ae
Fredric Laestander <fredriclaestander@gmail.com>
2025-10-27 22:46:42 +0100

    add loader on button component

--- apps/frontend/src/components/button.tsx
+++ apps/frontend/src/components/button.tsx
@@ -1,4 +1,4 @@
-import type { LucideIcon } from 'lucide-react'
+import { LoaderCircle, type LucideIcon } from 'lucide-react'
 import type { ReactNode } from 'react'
 import { cn } from '../utils/classname'
 
@@ -7,6 +7,7 @@ type Base = {
   variant?: 'primary' | 'secondary'
   children: ReactNode
   classname?: string
+  isLoading?: boolean
 }
 
 type ButtonProps = {
@@ -27,6 +28,7 @@ export const Button = ({
   onClick,
   type,
   classname,
+  isLoading,
 }: Props) => {
   return (
     <Element
@@ -34,12 +36,15 @@ export const Button = ({
       onClick={onClick}
       type={type}
       className={cn(
-        `flex cursor-pointer gap-3 rounded-full px-8 py-3 text-base font-bold whitespace-nowrap transition ${variant === 'secondary' ? 'hover:bg-grada-blue-950 bg-gray-950 text-gray-100' : 'bg-grada-blue-500 hover:bg-grada-blue-600 text-gray-950'}`,
+        `relative flex cursor-pointer gap-3 rounded-full px-8 py-3 text-base font-bold whitespace-nowrap transition ${variant === 'secondary' ? 'hover:bg-grada-blue-950 bg-gray-950 text-gray-100' : 'bg-grada-blue-500 hover:bg-grada-blue-600 text-gray-950'}`,
         classname,
       )}
     >
       {Icon && <Icon size={24} />}
-      {children}
+      <span className={cn(isLoading && 'text-transparent')}>{children}</span>
+      {isLoading && (
+        <LoaderCircle className="absolute inset-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-spin" />
+      )}
     </Element>
   )
 }
