871ee96e55feadb92b41d9122242da87f2c287d5
Fredric Laestander <fredriclaestander@gmail.com>
2025-11-05 00:15:17 +0100

    feat: create table design

---
+++ apps/frontend/src/components/admin/tableHeader.tsx
@@ -0,0 +1,10 @@
+export const TableHeader = () => {
+  return (
+    <div className="flex w-full items-center bg-gray-800 text-gray-400">
+      <div className="flex-1 truncate">Id</div>
+      <div className="flex-1 truncate">Name</div>
+      <div className="flex-1 truncate">Created at</div>
+      <div className="flex-1 truncate">Updated at</div>
+    </div>
+  )
+}
